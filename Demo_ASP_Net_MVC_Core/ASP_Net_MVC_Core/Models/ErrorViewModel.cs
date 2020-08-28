f551e6e16e06bb6f6bf73827a5a44e2d9fccf8f4
Thomas Hedegaard <thomashj93@gmail.com>
2020-08-28 07:00:34 +0200

    ASP core seed

---
+++ Demo_ASP_Net_MVC_Core/ASP_Net_MVC_Core/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASP_Net_MVC_Core.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
